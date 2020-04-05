5467f74678439036c22e0aaca70bdb0283d0498f
Imane <imaneelmouradi98@gmail.com>
2020-04-05 17:20:17 +0100

    add: context to store globalData

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { ProductProvider } from "./components/globalData/Context";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
