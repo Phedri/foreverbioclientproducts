ffb24988b6bc4bcab075bb4e0b120bc955f315ba
Imane <imaneelmouradi98@gmail.com>
2020-04-05 14:44:24 +0100

    add: advertising

---
+++ src/components/layout/Advertising.js
@@ -0,0 +1,186 @@
+import React, { Component } from "react";
+
+export default class Advertising extends Component {
+  render() {
+    return (
+      <div class="ps-banner">
+        <div class="rev_slider fullscreenbanner" id="home-banner">
+          <ul>
+            <li
+              class="ps-banner"
+              data-index="rs-2972"
+              data-transition="random"
+              data-slotamount="default"
+              data-hideafterloop="0"
+              data-hideslideonmobile="off"
+              data-rotate="0"
+            >
+              <img
+                class="rev-slidebg"
+                src="images/slider/3.jpg"
+                alt=""
+                data-bgposition="center center"
+                data-bgfit="cover"
+                data-bgrepeat="no-repeat"
+                data-bgparallax="5"
+                data-no-retina
+              />
+              <div
+                class="tp-caption ps-banner__header"
+                id="layer-1"
+                data-x="left"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-150','-120','-150','-170']"
+                data-width="['none','none','none','400']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p>
+                  Lorem ipsum <br />
+                  Lorem
+                </p>
+              </div>
+              <div
+                class="tp-caption ps-banner__title"
+                id="layer21"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-60','-40','-50','-70']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1200,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p class="text-uppercase">PUBLICITÉ 1</p>
+              </div>
+              <div
+                class="tp-caption ps-banner__description"
+                id="layer211"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['30','50','50','50']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1200,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  <br />
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. <br />
+                  Eleifend quam adipiscing vitae proin sagittis nisl.
+                </p>
+              </div>
+              <a
+                class="tp-caption ps-btn"
+                id="layer31"
+                href="#"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['120','140','200','200']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                Acheter maintenant!<i class="ps-icon-next"></i>
+              </a>
+            </li>
+            <li
+              class="ps-banner ps-banner--white"
+              data-index="rs-100"
+              data-transition="random"
+              data-slotamount="default"
+              data-hideafterloop="0"
+              data-hideslideonmobile="off"
+              data-rotate="0"
+            >
+              <img
+                class="rev-slidebg"
+                src="images/slider/2.jpg"
+                alt=""
+                data-bgposition="center center"
+                data-bgfit="cover"
+                data-bgrepeat="no-repeat"
+                data-bgparallax="5"
+                data-no-retina
+              />
+              <div
+                class="tp-caption ps-banner__header"
+                id="layer20"
+                data-x="left"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-150','-120','-150','-170']"
+                data-width="['none','none','none','400']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p>
+                  Lorem Ipsum
+                  <br />
+                  Lorem
+                </p>
+              </div>
+              <div
+                class="tp-caption ps-banner__title"
+                id="layer339"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['-60','-40','-50','-70']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1200,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p class="text-uppercase">PUBLICITÉ 2 </p>
+              </div>
+              <div
+                class="tp-caption ps-banner__description"
+                id="layer2-14"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['30','50','50','50']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1200,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                <p>
+                  Amet cursus sit amet dictum sit amet justo donec enim
+                  <br />
+                  Nec feugiat in fermentum posuere urna. Aliquet lectus <br />
+                  proin nibh nisl condimentum. Hendrerit dolor magna eget est.
+                </p>
+              </div>
+              <a
+                class="tp-caption ps-btn"
+                id="layer364"
+                href="#"
+                data-x="['left','left','left','left']"
+                data-hoffset="['-60','15','15','15']"
+                data-y="['middle','middle','middle','middle']"
+                data-voffset="['120','140','200','200']"
+                data-type="text"
+                data-responsive_offset="on"
+                data-textAlign="['center','center','center','center']"
+                data-frames='[{"delay":1500,"speed":1500,"frame":"0","from":"x:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"x:50px;opacity:0;","ease":"Power3.easeInOut"}]'
+              >
+                Acheter maintenant!<i class="ps-icon-next"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
